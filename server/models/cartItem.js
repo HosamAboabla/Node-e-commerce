5486bb1ec859a05c268352cb51295c0c60ef07fd
Galal <galal@gmail.com>
2022-03-24 19:12:02 +0200

    cartItem model

---
+++ server/models/cartItem.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CartItemSchema = new Schema(
+    {
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: 'product'
+        },
+        quantity: Number,
+        purchasePrice: {
+            type: Number,
+            default: 0
+        },
+        totalPrice: {
+            type: Number,
+            default: 0
+        },
+        status: {
+            type: String,
+            default: 'Not processed',
+            enum: ['Not processed', 'Processing', 'Shipped', 'Delivered', 'Cancelled']
+        }
+    });
+
+module.exports = mongoose.model('cartItem', CartItemSchema);
