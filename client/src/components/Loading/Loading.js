0ffa7857b13ed791cd5aa62d294e34ffad60be04
abuelnasr0 <abuelnasr70@gmail.com>
2022-04-14 23:44:30 +0200

    Loading

---
+++ client/src/components/Loading/Loading.js
@@ -0,0 +1,17 @@
+import './loading.css'
+const Loading = () => {
+    return ( 
+        <div className="loadingDiv">
+            <div className="square">
+                    <div className="c1"></div>
+                    <div className="c2"></div>
+                    <div className="c3"></div>
+                    <div className="c4"></div>
+                    <div className="c5"></div>
+                    <div className="c6"></div>
+                    <div className="c7"></div>
+            </div>
+        </div>
+    );
+}
+export default Loading;
