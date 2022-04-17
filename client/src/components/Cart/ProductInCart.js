69314ed877b5448226b3f0326e9477a982715d98
HosamAboabla <hosamaboabla@yahoo.com>
2022-04-17 15:44:40 +0200

    Adding remove product from cart functionality

--- client/src/components/Cart/ProductInCart.js
+++ client/src/components/Cart/ProductInCart.js
@@ -1,10 +1,16 @@
 import useFetch from "../../useFetch";
 import Loading from "../Loading/Loading";
 
-const ProductInCart = ({product_id , quantity , totalPrice , setTotalPrice}) => {
+const ProductInCart = ({product_id , quantity , totalPrice , setTotalPrice , cart , setCart}) => {
 
     const link = `/api/products/list/${product_id}`;
     const {data:product , error , isPending} = useFetch(link);
+
+    const removeCartItem = () => {
+        setCart(cart.filter(item => item.product_id !== product_id));
+       
+    }
+
     
     return ( 
     <div className="product2">
@@ -17,7 +23,7 @@ const ProductInCart = ({product_id , quantity , totalPrice , setTotalPrice}) => 
                     <h3 className="product2-name">{product.name}</h3>
                     <h4 className="product2-price">{product.price}</h4>
                     <span className="product2-quantity">Quantity: </span>{quantity}
-                    <p className="product2-remove">
+                    <p onClick={removeCartItem} className="product2-remove">
                         <i className="fa fa-trash" aria-hidden="true"></i>
                         <span className="remove">Remove</span>
                     </p>
