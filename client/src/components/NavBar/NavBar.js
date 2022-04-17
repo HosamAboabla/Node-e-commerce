cfb0b90e94cbd2e359c38c21ab3063ae41c87b40
abuelnasr0 <abuelnasr70@gmail.com>
2022-04-17 16:46:29 +0200

    Navbar to be goood in mobile

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import './NavBarStyle.css'
 const NavBar = () => {
   return (
     <div className="topnav">
-        <a  className="name" href="/">E-commerce</a>
+        <a  className="name" href="/">LOGO</a>
         <div  className='Form'>
             <input  type="text" placeholder="Search.." name="search" />
             <button  ><i className="fa fa-search"></i></button>
