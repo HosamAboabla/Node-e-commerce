206d024f805fb724cd097923ff20e1843b2f09f0
HosamAboabla <hosamaboabla@yahoo.com>
2022-04-25 22:24:16 +0200

    adding authentication

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -14,7 +14,7 @@ function creatJWToken(id,isAdmin) {
 }
 
 function errorHandller(err) {
-    console.log(err.message, err.code)
+    console.log("message" , err.message, err.code)
     let errors = {}
     if (err.message.includes("User validation failed"))
     {
@@ -44,8 +44,7 @@ router.post('/register' , async(request,responce) => {
                 firstName: request.body.firstName,
                 lastName : request.body.lastName,
                 phoneNumber: request.body.phone,
-                isAdmin : request.body.isAdmin,
-                password : cryptoJS.AES.encrypt(request.body.password,process.env.password_sec).toString(),
+                password : request.body.password && cryptoJS.AES.encrypt(request.body.password,process.env.password_sec).toString(),
                 // address : [request.body.address1,request.body.address2]
                 // address : request.body.address
         });
@@ -53,12 +52,12 @@ router.post('/register' , async(request,responce) => {
         // creating access token
         const accessToken = creatJWToken(newUser._id,newUser.isAdmin)
         responce.cookie('jwt', accessToken, {httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 2})
-        const {password, ...others} = newUser._doc
-        responce.status(201).json({Message : `new user was created:`,Data: {...others,accessToken}})
+        responce.status(201).json({Message : `new user was created:`,Data: {accessToken}})
     } catch(err) {
+        // console.log("Errors :" , err);
         errors = errorHandller(err)
-        console.log(errors)
-        console.log("there was error in register")
+        // console.log(errors)
+        // console.log("there was error in register")
         responce.status(500).json({Message: 'There was an ERROR creating the user',Error: errors})
     }
 })
@@ -74,12 +73,10 @@ router.post('/login' , async(request,responce) => {
         if (unHashed !== request.body.password){
         responce.status(401).json({passwordError : 'Invalid password'})
         }else {
-        const {password, ...others} = user._doc
-
         // creating access token
         const accessToken = creatJWToken(user._id,user.isAdmin)
         responce.cookie('jwt', accessToken, {httpOnly: true, maxAge:1000 * 60 * 60 * 24 * 2})
-        responce.status(200).json({success : true, Data: {...others,accessToken}})
+        responce.status(200).json({success : true, Data: { accessToken }})
     }}
     } catch(err) {
         errors = errorHandller(err)
