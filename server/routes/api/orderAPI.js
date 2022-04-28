568d029971b594720552ef7f7e7f75a975d5d0f4
Galal <galalnasser5@gmail.com>
2022-04-28 02:17:50 +0200

    modifying api

--- server/routes/api/orderAPI.js
+++ server/routes/api/orderAPI.js
@@ -1,13 +1,12 @@
-const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 const Orders = require('../../models/order.js');
 const { verify, verifyAndAuthorization, verifyAndAdmin } = require('../verifyToken')
 
 
-router.get('/user/:user.id', async (req, res) => {
+router.get('/user/:id', async (req, res) => {
     try {
-        const userOrder = await Orders.find().where('user').equals(request.body.userid);
+        const userOrder = await Orders.find().where('user').equals(req.params.id);
         res.status(200).json(userOrder);
     } catch (err) {
         res.status(500).json({ Message: 'there was an ERROR ', ERROR: err });
@@ -18,9 +17,9 @@ router.get('/user/:user.id', async (req, res) => {
 router.post('/create', async (req, res) => {
     try {
         const newOrder = new Orders({
-            cart: request.body.cart,
-            user: request.body.user,
-            total: request.body.total
+            cart: req.body.cart,
+            user: req.body.user,
+            total: req.body.total
         });
         await newOrder.save();
         res.status(201).send(`new order is created:  ${newOrder}`);
@@ -32,7 +31,7 @@ router.post('/create', async (req, res) => {
 
 router.delete('/delete/:id', verifyAndAdmin, async (req, res) => {
     try {
-        const removed = await Orders.deleteOne({ _id: request.params.id });
+        const removed = await Orders.deleteOne({ _id: req.params.id });
         res.status(200).json(removed);
     } catch (err) {
         res.status(500).json({ Message: "The product hasn't been deleted", Error: err });
